8ad389cb2fac822404bbb7c4524aae5ba8ab97a5
Tatiane65 <tatycordeiro72@gmail.com>
2026-08-01 10:04:22 -0300

    Add mainPrecos.jsx to render Precos component

---
+++ src/src/mainPrecos.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Precos from "./Precos.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Precos />
+  </React.StrictMode>
+);
