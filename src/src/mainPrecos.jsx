8c0f320d3249e77c9c8449692bb9d2b382df7d9e
Tatiane65 <tatycordeiro72@gmail.com>
2026-08-01 16:00:13 -0300

    Delete src/src directory

--- src/src/mainPrecos.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Precos from "./Precos.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Precos />
-  </React.StrictMode>
-);
