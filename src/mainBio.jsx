ea2aed1c54ca6503c015c007a428057c983d6d86
Tatiane65 <tatycordeiro72@gmail.com>
2026-08-31 18:38:45 -0300

    Add mainBio component to render LinkBio

---
+++ src/mainBio.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LinkBio from "./LinkBio.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LinkBio />
+  </React.StrictMode>
+);
